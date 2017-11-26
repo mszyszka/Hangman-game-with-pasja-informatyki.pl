432dcdd4560ca3bd66e8c696080ca0fd0045552f
Michał Szyszka <mszyszka223@gmail.com>
2017-11-26 09:04:36 +0100

    I added array with alphabet

--- script.js
+++ script.js
@@ -16,12 +16,53 @@ function wypiszHaslo() {
 }
 window.onload = start;
 
+var lettersArry = new Array(35);
+lettersArry[0] = "A";
+lettersArry[1] = "Ą";
+lettersArry[2] = "B";
+lettersArry[3] = "C";
+lettersArry[4] = "Ć";
+lettersArry[5] = "D";
+lettersArry[6] = "E";
+lettersArry[7] = "Ę";
+lettersArry[8] = "F";
+lettersArry[9] = "G";
+lettersArry[10] = "H";
+lettersArry[11] = "I";
+lettersArry[12] = "J";
+lettersArry[13] = "K";
+lettersArry[14] = "L";
+lettersArry[15] = "Ł";
+lettersArry[16] = "M";
+lettersArry[17] = "N";
+lettersArry[18] = "Ń";
+lettersArry[19] = "O";
+lettersArry[20] = "Ó";
+lettersArry[21] = "P";
+lettersArry[22] = "Q";
+lettersArry[23] = "R";
+lettersArry[24] = "S";
+lettersArry[25] = "Ś";
+lettersArry[26] = "T";
+lettersArry[27] = "U";
+lettersArry[28] = "V";
+lettersArry[29] = "W";
+lettersArry[30] = "X";
+lettersArry[31] = "Y";
+lettersArry[32] = "Z";
+lettersArry[33] = "Ż";
+lettersArry[34] = "Ź";
+
+
 function start(){
     
     var trescDiva = "";
     
     for(i=0; i<=34; i++){
-        trescDiva = trescDiva + '<div class="litera">A</div>';
+        trescDiva = trescDiva + '<div class="litera">'+lettersArry[i]+'</div>';
+        if( ((i+1)%7)==0) {
+            trescDiva = trescDiva + '<div class="clearfix"></div>';
+        }
     }
     
     document.getElementById("alfabet").innerHTML = trescDiva;
